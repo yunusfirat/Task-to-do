6371845880d95516485027fe906e3877ad9aa561
yunusfirat <yunusfirat44@gmail.com>
2021-02-16 13:17:36 +0000

    User Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #read all tasks
+   get '/tasks', to: "tasks#index"
 end
